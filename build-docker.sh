af45f14eae24e848115f9f8499d4b8d03e7f58ac
Reza Mohammadi <rmm.dev.backend@gmail.com>
2023-09-29 20:50:36 +0330

    Update building docker command

--- build-docker.sh
+++ build-docker.sh
@@ -1,1 +1,1 @@
-docker build --build-arg http_proxy=http://192.168.1.132:8118 --build-arg https_proxy=http://192.168.1.132:8118 --tag=obj-02 .
+docker build --build-arg http_proxy=http://192.168.1.132:8118 --build-arg https_proxy=http://192.168.1.132:8118 --tag=obj-04 .
