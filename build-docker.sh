075161b77ee9f4ff15f907f88feca0bb85f4be9b
Reza Mohammadi <rmm.dev.backend@gmail.com>
2023-10-23 21:04:21 +0330

    Update docker compose versions

--- build-docker.sh
+++ build-docker.sh
@@ -1,1 +1,1 @@
-docker build --shm-size 4gb --build-arg http_proxy=http://192.168.1.132:8118 --build-arg https_proxy=http://192.168.1.132:8118 --tag=obj-04 .
+docker build --shm-size 4gb --build-arg http_proxy=http://192.168.1.132:8118 --build-arg https_proxy=http://192.168.1.132:8118 --tag=trk-01 .
